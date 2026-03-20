c1ea835d475a23193e3242a63198cfd0c3ad493d
Sagar Dahiya <dahiyabadshah.fossil@gmail.com>
2026-03-20 19:04:28 +0530

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -4,8 +4,11 @@ read -p "Enter file name to save: " NAME
 
 echo "Paste the url and press Enter:"
 read -r URL
+
 echo "URL is $URL"
-echo "Location is $NAME"
+
+echo "File Name is $NAME"
+
 /usr/bin/n-m3u8dl-re "$URL" \
     -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0" \
     -H "Referer: https://player.hotmart.com/" \
