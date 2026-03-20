d628b68fa183fce14e107c3d26fda9fbec7428e9
Sagar Dahiya <dahiyabadshah.fossil@gmail.com>
2026-03-20 19:26:58 +0530

    Added Colors

--- dl.sh
+++ dl.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Save this as 'dl.sh'
+
+# Enter Name
 read -p "Enter file name to save: " NAME
 
+# Add url
 echo "Paste the url and press Enter:"
 read -r URL
 
-echo "URL is $URL"
+echo -e "\e[32mURL is $URL\e[0m"
 
-echo "File Name is $NAME"
+echo -e  "\e[32mFile Name is $NAME\e[0m"
 
 /usr/bin/n-m3u8dl-re "$URL" \
     -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0" \
